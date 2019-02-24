8ee714ba14f0cc0fde49acf399595620fc007afb
rhaz96 <43581750+rhaz96@users.noreply.github.com>
2019-02-24 18:28:03 -0500

    Update CaseStudy1Script.R
    Removed unnecessary code and simplified code using the dplyr library.  Added comments to each code chunk.

--- CaseStudy1Script.R
+++ CaseStudy1Script.R
@@ -1,55 +1,38 @@
-library(tidyverse)
-library(plyr)
-library(psych)
-library(doBy)
-library(pastecs)
+library(ggplot2)
 library(dplyr)
 
-
-getwd()
-setwd("c:/Users/jhold/Desktop/case study 1")
-beers <- read_csv("c:/Users/jhold/Desktop/case study 1/Beers.csv")
-brewers <- read_csv("c:/Users/jhold/Desktop/case study 1/Breweries.csv")
+# load "Beers.csv" - call it beers
+# load "Brewers.csv" - call it brewers
 str(beers)
 str(brewers)
 head(beers)
-BrewersbyState <- brewers$State
-CntState <- count(BrewersbyState)
-CntState <- CntState[order(-CntState$freq),] 
-str(CntState)
-TotCNT <- sum(CntState$freq)
+BrewersbyState <-  brewers %>% group_by(State) %>% summarise(TotalBreweries = length(State))
 Brewtot <- merge(beers, brewers, by.x = "Brewery_id", by.y = "Brew_ID")
-cntbeers <- count(Brewtot$State)
-cntbeers <- cntbeers[order(-cntbeers$freq),] 
-totbeers <- sum(cntbeers$freq)
-str(Brewtot)
+
+# rename Name.x, Name.y columns
+colnames(Brewtot)[c(2,8)] = c("Beer", "Brewer")
 head(Brewtot)
 tail(Brewtot)
 MissTot <- sapply(Brewtot, function(x) sum(is.na(x)))
-describeBy(Brewtot$ABV,Brewtot$State)
-MedA <- aggregate (Brewtot$ABV, list(Brewtot$State), median, na.rm=TRUE)
-str(MedA)
-ggplot(data=MedA, aes(x=reorder(Group.1, x), y=x, fill=Group.1)) +geom_bar(stat="identity") +coord_flip() +ylab("Median ABV") + xlab("State Name") +ggtitle("Median ABV by State")+ theme(plot.title = element_text(hjust = 0.5)) + theme(legend.position="none")
-MedI <- aggregate (Brewtot$IBU, list(Brewtot$State), median, na.rm=TRUE)
-MedI <- MedI[-c(42),]
-ggplot(data=MedI, aes(x=reorder(Group.1, x), y=x, fill=Group.1)) +geom_bar(stat="identity") +coord_flip() +ylab("Median IBU") + xlab("State Name") +ggtitle("Median IBU by State")+ theme(plot.title = element_text(hjust = 0.5)) + theme(legend.position="none")
-hist(Brewtot$ABV, main="Distribution of ABV", xlab="Alcohol by Volume", border="black", col="blue", xlim=c(0,.13))
-hist(Brewtot$IBU, main="Distribution of IBU", xlab="International Bitterness Units", border="black", col="red", xlim=c(0,100))
-describe(Brewtot$ABV, na.rm = TRUE,quant=c(.05,.10,.25,.75,.90,.95))
-describe(Brewtot$IBU, na.rm = TRUE,quant=c(.05,.10,.25,.75,.90,.95))
-summary(Brewtot$ABV)
-Brewtot <- Brewtot[order(-Brewtot$ABV),] 
-Brewtot <- Brewtot[order(-Brewtot$IBU),] 
-head(Brewtot)
-MedA <- MedA[order(-MedA$x),] 
-options(scipen=100)
-stat.desc(Brewtot$ABV)
-stat.desc(Brewtot$IBU)
-MedI <- MedI[order(-MedI$x),] 
-ggplot(data=Brewtot, aes(x=ABV, y=IBU)) +geom_point(shape = 16, size = 5) +ylab("IBU") + xlab("ABV") +ggtitle("Craft Beer IBU by ABV")+ theme(plot.title = element_text(hjust = 0.5))
-ggplot(data=Brewtot, aes(x=IBU, y=ABV)) +geom_point(shape = 16, size = 2, color="blue") + stat_smooth(method = 'lm', color='red') + labs(title = 'Craft Beer IBU by ABV') + theme(plot.title = element_text(hjust = 0.5))
-cor.test( ~ ABV + IBU,data=Brewtot,method = "pearson")
-model = lm(IBU ~ ABV, data = Brewtot)
-summary(model)
 
+alcohol_and_bitterness <- Brewtot %>% group_by(State) %>% 
+  summarise(MedianAlcohol = median(ABV, na.rm = T), MedianBitter = median(IBU, na.rm = T))
+# SD still has a NA since there's no information on bitterness for any of their breweries
+# remove SD
+alcohol_and_bitterness <- alcohol_and_bitterness[-42,]
 
+# summarising median bitterness and alcohol content by State, and plotting the results                  
+ggplot(alcohol_and_bitterness) + 
+  geom_col(aes(x = reorder(State, MedianAlcohol), y = MedianAlcohol), fill = 'steel blue') + coord_flip() + xlab("Median ABV") + ylab("State") + ggtitle("Median Beer Alcohol Content by State") + theme(plot.title = element_text(hjust = 0.5))
+ggplot(alcohol_and_bitterness) + 
+  geom_col(aes(x = reorder(State, MedianBitter), y = MedianBitter), fill = 'orange') + coord_flip() + xlab("Median IBU") + ylab("State") + ggtitle("Median Beer Bitterness by State") + theme(plot.title = element_text(hjust = 0.5))
+
+# finding the state with the max beer ABV and state with max IBU
+max_ABV_state = Brewtot[which(Brewtot$ABV == max(Brewtot$ABV, na.rm = T)), 10]
+max_IBU_state = Brewtot[which(Brewtot$IBU == max(Brewtot$IBU, na.rm = T)), 10]
+summary(Brewtot$ABV)                 
+
+# looking for a linear correlation between IBU and ABV                  
+ggplot(Brewtot) + geom_point(aes(ABV, IBU)) + ggtitle("Craft Beer IBU by ABV")+
+  theme(plot.title = element_text(hjust = 0.5))
+cor.test(~ ABV + IBU, data = Brewtot, method = "pearson")
